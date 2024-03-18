adef9ae25c27cf78866dec1ac3c14e3c33b1ec98
Maheshwarreddy970 <mr8382455@gmail.com>
2024-03-18 20:16:23 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['chatdocai.s3.ap-south-1.amazonaws.com'],
+        domains: ['chatdocai.s3.ap-south-1.amazonaws.com','img.clerk.com'],
       },
 };
 
--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -276,18 +276,19 @@ const MenuOptions = ({
                       val = <result.path />
                     }
                     return (
-                      <CommandItem
+                      <button
                         key={sidebarOptions.id}
                         className="md:w-[320px] w-full"
                       >
+                        
                         <Link
                           href={sidebarOptions.link}
                           className="flex items-center gap-2 hover:bg-transparent rounded-md transition-all md:w-full w-[320px]"
                         >
                           {val}
                           <span>{sidebarOptions.name}</span>
-                        </Link>
-                      </CommandItem>
+                        </Link>                    
+                      </button>
                     )
                   })}
                 </CommandGroup>
