2dec41fb91da2daf442af3c17982fce5d12a0df4
Maheshwarreddy970 <mr8382455@gmail.com>
2024-03-26 14:52:14 +0530

    dashboard

--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -277,7 +277,7 @@ const MenuOptions = ({
                     return (
                       <CommandItem
                         key={sidebarOptions.id}
-                        className="px-4 py-2 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md "
+                        className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md "
                       >
                         
                         <Link
